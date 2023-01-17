0c64988d988f305b5852e3b28a88a3c4d9055c78
GabrielaMeza93 <gabrielameza1993@gmail.com>
2023-01-17 00:58:10 -0300

    agrego formulario

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')))
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')))
 
 
 
